d8489b66ea601a8c1743957ed59832e983a346aa
Pablo <pabloimrik17@hotmail.com>
2017-01-20 23:36:33 +0100

    Creando clases y estructura del proyecto.

---
+++ Assets/Scripts/GeneradorOndas.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradorOndas : MonoBehaviour {
+	[SerializeField]
+	private int numeroOndasRestantes;
+
+	private float tiempoEntreOnda;
+	private float fuerzaImpulso;
+	private float delayInicial;
+	private float instanteCreacionGeneradorOndas;
+	private float instanteUltimaOndaLanzada;
+
+	private int ondasLanzadas;
+
+	public int NumeroOndasRestantes {
+		get {
+			return this.numeroOndasRestantes;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		this.instanteCreacionGeneradorOndas = 0;
+		this.instanteUltimaOndaLanzada = 0;
+		this.ondasLanzadas = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private bool HeLanzadoAlgunaOnda() {
+			
+
+		return this.ondasLanzadas == 0;
+	} 
+	
+
+}
