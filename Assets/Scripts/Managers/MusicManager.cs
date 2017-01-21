3f8d63d3e89520162de871f6f8225f4c2b32aa23
Pablo <pabloimrik17@hotmail.com>
2017-01-21 15:00:32 +0100

    Prefab Barco, Generador de Ondas, Onda, ExitBeacon

--- Assets/Scripts/Managers/MusicManager.cs
+++ Assets/Scripts/Managers/MusicManager.cs
@@ -1,16 +1,60 @@
 ﻿using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class MusicManager : Singleton<MusicManager> {
+	[SerializeField]
+	private const float _MASTER_VOLUME_ = 10;
 
-	// Use this for initialization
-	void Start () {
-		
+	private const string _MUSIC_ROOT_DIRECTORY = "Assets/Music/";
+
+	private AudioSource audioSource;
+
+	[SerializeField]
+	private Dictionary<string, AudioClip> AudioResourcesCollection;
+
+
+	void Awake() {
+		LoadAudioResources();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	// Use this for initialization
+	void Start() {
+		audioSource = gameObject.GetComponent<AudioSource> ();
+		SetVolume(_MASTER_VOLUME_);
+		playLevelAudio (0);
+	}
+
+	public void LoadAudioResources() {
+		AudioResourcesCollection
+	}
+
+	void OnLevelWasLoaded(int nivel) {
+		SetVolume(_MASTER_VOLUME_);
+		playLevelAudio ("ThemeNivel"+nivel);
+	}
+
+	private void playLevelAudio(string audioIndex) {   
+		if (AudioResourcesCollection.ContainsKey(audioIndex)) {
+			AudioClip thisLevelAudio = AudioResourcesCollection [audioIndex];
+			audioSource.clip = thisLevelAudio;
+			audioSource.loop = true;
+			audioSource.Play ();
+		}
+	}
+
+	private void playLevelAudio(int audioIndex) {   
+		if (AudioResourcesCollection.ContainsKey(AudioResourcesCollection.Keys.ElementAt(audioIndex))) {
+			AudioClip thisLevelAudio = AudioResourcesCollection.Values.ElementAt (audioIndex);
+			audioSource.clip = thisLevelAudio;
+			audioSource.loop = true;
+			audioSource.Play ();
+		}
+	}
+
+
+	public void SetVolume(float volume) {
+		audioSource.volume = volume;
 	}
 }
