48a735127ee9af5cd82832dc8fa6bc89c88c9dba
Pablo <pabloimrik17@hotmail.com>
2017-02-15 15:55:20 +0100

    Integración de Ads (por probar en casa cambiando de target platform)

---
+++ Assets/AdsController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+#if UNITY_ADS
+using UnityEngine.Advertisements;
+#endif
+
+public class UnityAdsExample : MonoBehaviour
+{
+
+	[SerializeField] private string androidGameId = "1308711"; //ID for testing
+	[SerializeField] private string iosGameId = "1308712"; //ID for testing
+
+	[SerializeField] bool enableTestMode = true;
+
+	void Start ()
+	{
+		string gameId = null;
+
+		#if UNITY_IOS // If build platform is set to iOS...
+		gameId = iosGameId;
+		#elif UNITY_ANDROID // Else if build platform is set to Android...
+		gameId = androidGameId;
+		#endif
+
+		#if UNITY_ADS
+		if (string.IsNullOrEmpty(gameId)) { // Make sure the Game ID is set.
+			Debug.LogError("Failed to initialize Unity Ads. Game ID is null or empty.");
+		} else if (!Advertisement.isSupported) {
+			Debug.LogWarning("Unable to initialize Unity Ads. Platform not supported.");
+		} else if (Advertisement.isInitialized) {
+			Debug.Log("Unity Ads is already initialized.");
+		} else {
+			Debug.Log(string.Format("Initialize Unity Ads using Game ID {0} with Test Mode {1}.",
+				gameId, enableTestMode ? "enabled" : "disabled"));
+			Advertisement.Initialize(gameId, enableTestMode);
+			if(Advertisement.IsReady()) {
+				Advertisement.Show ();
+			}
+		}
+		#endif
+	}
+}
