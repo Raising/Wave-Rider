b63c3351bf6ea719da752b055ee95b76462497fb
Pablo <pabloimrik17@hotmail.com>
2017-02-16 16:32:09 +0100

    Integrando compras en app

--- Assets/Scripts/Utils/AdsController.cs
+++ Assets/Scripts/Utils/AdsController.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
+#if UNITY_ADS
 using UnityEngine.Advertisements;
+#endif
 
 public class AdsController : MonoBehaviour {
+    #if UNITY_ADS
     public void ShowAd() {
         if (Advertisement.IsReady()) {
             Advertisement.Show();
         }
     }
+    #endif
 }
